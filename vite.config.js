03c34392e913d00839e8db8e454a65ffb4c605c4
rpzgy <zhou_guoyan@rp.edu.sg>
2025-04-06 17:31:36 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/zgy.github.io/',
+// })
+
+
+
 export default defineConfig({
   plugins: [react()],
   base: '/zgy.github.io/',
-})
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',  // Organize assets in a subfolder
+    emptyOutDir: true,    // Clear dist folder before build
+  }
+});
